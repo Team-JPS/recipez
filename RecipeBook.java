12f1aa42a87cafdb7ce4ad800a708eef51fd05bc
BearXDown <148283386+BearXDown@users.noreply.github.com>
2025-02-28 14:26:38 -0700

    Add files via upload

---
+++ RecipeBook.java
@@ -0,0 +1,17 @@
+
+import java.util.ArrayList;
+
+public class RecipeBook {
+    private ArrayList<Recipe> recipes;
+
+    public RecipeBook() {
+        this.recipes = new ArrayList<>();
+    }
+
+    public Arraylist<recipe> getRecipes() {
+        return this.recipes;
+    }
+    
+}
+
+
